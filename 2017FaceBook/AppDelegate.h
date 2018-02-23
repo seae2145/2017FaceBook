5baf3aa117df9887dbc4734b6e882e2f0c26f2a2
張揚法 <nttu94413@yahoo.com.tw>
2018-02-23 13:38:20 +0800

    Initial Commit

---
+++ 2017FaceBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2017FaceBook
+//
+//  Created by 張揚法 on 2018/2/23.
+//  Copyright © 2018年 張揚法. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2017FaceBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2017FaceBook
+//
+//  Created by 張揚法 on 2018/2/23.
+//  Copyright © 2018年 張揚法. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
